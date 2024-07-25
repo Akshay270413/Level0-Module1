524f48b5e587db93aabc467726b868a98b267632
league <bkartho@gmail.com>
2024-07-25 10:29:00 -0700

    riddler

--- _04_int/_1_riddler/riddler.py
+++ _04_int/_1_riddler/riddler.py
@@ -1,3 +1,13 @@
+from tkinter import Tk, simpledialog, messagebox
+
+window = Tk()
+window.withdraw()
+name = simpledialog.askstring(title='greeter', prompt="Mr and Mrs mustard had 5 daughters. Each daughter had 1 brother. how many people are in the mustard family?")
+if name== "9":
+    messagebox.showinfo(title='greeter', message="nice job")
+else:
+    messagebox.showinfo(title='greeter', message="Wrong")
+
 """
 * Write a python program that asks the user a minimum of 3 riddles.
 
